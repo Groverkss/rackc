235406e4225dd67b869cd8d83d49445d8c67a4ad
Groverkss <groverkss@gmail.com>
2022-02-19 20:15:01 +0530

    Add opt.rkt

---
+++ opt.rkt
@@ -0,0 +1,21 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require "utilities.rkt")
+(require "interp-Lvar.rkt")
+(require "interp-Cvar.rkt")
+(require "interp.rkt")
+(require "compiler.rkt")
+
+(define file (command-line #:args (filename) filename))
+(define ast (read-program file))
+
+(define (opt passes ast)
+  (println ast)
+  (match passes
+    ['() ast]
+    [(list (list name fun interp) more ...)
+     (println (string-append "Applying " name))
+     (opt more (fun ast))]))
+
+(define final (opt compiler-passes ast))
