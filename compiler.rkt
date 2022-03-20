4d2a38e13e1cd9a81e32808492f2bd8bb81f98dc
Groverkss <groverkss@gmail.com>
2022-03-21 03:45:22 +0530

    Implement liveness analysis and interference graph

--- compiler.rkt
+++ compiler.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require racket/set racket/stream)
 (require racket/fixnum)
+(require graph)
 (require "interp-Lint.rkt")
 (require "interp-Lvar.rkt")
 (require "interp-Cvar.rkt")
@@ -197,9 +198,9 @@
       (Instr 'movq (list (select-atm e1) x))
       (Instr (get-op-name e) (list (select-atm e2) x)))]
     [(Prim 'read '())
-      (list
-        (Callq 'read_int 0)
-        (Instr 'movq (list (Reg 'rax) x)))]
+     (list
+      (Callq 'read_int 0)
+      (Instr 'movq (list (Reg 'rax) x)))]
     [else (error "select-assign unhandled case")]))
 
 (define (select-stmt stmt)
@@ -219,6 +220,108 @@
   (match p
     [(CProgram info (list (cons 'start t)))
      (X86Program info (list (cons 'start (Block '() (select-tail t)))))]))
+
+(define (convert-to-regs names) (for/list ([reg names]) (Reg reg)))
+(define caller-saved-list (convert-to-regs (list 'rax 'rcx 'rdx 'rsi 'rdi 'r8 'r9 'r10 'r11)))
+(define callee-saved-list (convert-to-regs (list 'rsp 'rbp 'rbx 'r12 'r13 'r14 'r15)))
+
+;; Get all write locations from an instruction.
+;; TODO: Ask Bharat if all instructions do a write or is it only
+;;       movq.
+(define (get-write-locations instr)
+  (match instr
+    [(Instr _ `(,arg1 ,arg2)) (set arg2)]
+    [(Instr _ `(,arg1)) (set arg1)]
+    [(Callq _ _) (list->set caller-saved-list)]
+    [(Jmp _) (set)]
+    [_ "Unimplemented instruction"]))
+
+;; Get all read locations from an instruction.
+(define (get-read-locations instr)
+  (set-subtract
+   (match instr
+     [(Instr _ (list args ...)) (list->set (filter (lambda (x) (not (Imm? x))) args))]
+     [(Callq _ airty) (list->set (take callee-saved-list airty))]
+     [(Jmp _) (set)]
+     [_ "Unimplemented instruction?"])
+   (get-write-locations instr)))
+
+(define (liveness-analysis-instrs instrs)
+  (match instrs
+    ; L_after is empty for last instruction.
+    [(list instr) (list (set))]
+    ; L_after(k) = (L_after(k + 1) - W(k + 1)) + R(k + 1)
+    [(list _ instrk+1 _ ...)
+     (let* ([anal (liveness-analysis-instrs (rest instrs))]
+            [L_afterk+1 (car anal)])
+       (cons
+        (set-union (set-subtract
+                    L_afterk+1
+                    (get-write-locations instrk+1))
+                   (get-read-locations instrk+1))
+        anal))]))
+
+;; Given a block, do a pass of backward dataflow analysis on it.
+(define (liveness-analysis block)
+  (match block
+    [(Block blkinfo instrs)
+     (Block
+      (dict-set blkinfo 'liveness (liveness-analysis-instrs instrs))
+      instrs)]))
+
+;; Uncover live pass : Analysis to find live variables.
+(define (uncover-live p)
+  (match p
+    [(X86Program info (list (cons 'start block)))
+     (X86Program info (list (cons 'start (liveness-analysis block))))]))
+
+; Add conflicts between set1 and set2
+(define (add-interference set1 set2 graph)
+  (for* ([s1 set1]
+         [s2 set2])
+    (add-edge! graph s1 s2))
+  graph)
+
+(define (build-interference-instr L_afterk instr graph)
+  (match instr
+    ; Add edge between (d, v) \forall v \in L_after(k) | v != s or v != d
+    [(Instr 'movq (list s d))
+     (let* ([filtered_L (set-subtract L_afterk (set s d))])
+       (add-interference filtered_L (set d) graph))]
+    ; For any other instruction, (d, v) \forall d \in write(instr) v \in L_afterk | v != d
+    [_
+     (let* ([write-locs (get-write-locations instr)]
+            [filtered_L (set-subtract L_afterk write-locs)])
+       (add-interference filtered_L write-locs graph))]))
+
+(define (build-interference-instrs liveness instrs graph)
+  (match instrs
+    ['() graph]
+    [(list instr rest-instrs ...)
+     (build-interference-instrs
+      (cdr liveness)
+      rest-instrs
+      (build-interference-instr (car liveness) instr graph))]))
+
+(define (build-interference-blocks blocks graph)
+  (match blocks
+    ['() graph]
+    [(list (cons _ (Block blkinfo instrs)) rest-blocks ...)
+     (build-interference-blocks
+      rest-blocks
+      (build-interference-instrs
+       (dict-ref blkinfo 'liveness) instrs graph))]))
+
+;; Build Interference Graph : Analysis to find register conflicts.
+(define (build-interference p)
+  (match p
+    [(X86Program info blocks)
+     (X86Program
+      (dict-set
+       info
+       'conflicts
+       (build-interference-blocks blocks (undirected-graph '())))
+      blocks)]))
 
 (define (get-next-stack-loc env)
   (* (- 8) (+ (length env) 1)))
@@ -294,19 +397,19 @@
 ; TODO: Fix stack frame size (Assumed to be 16?).
 (define (generate-prelude)
   (list (cons 'main (Block '()
-                     (list (Instr 'pushq (list (Reg 'rbp)))
-                           (Instr 'movq (list (Reg 'rsp) (Reg 'rbp)))
-                           (Instr 'subq (list (Imm 16000) (Reg 'rsp)))
-                           (Jmp 'start))))))
+                           (list (Instr 'pushq (list (Reg 'rbp)))
+                                 (Instr 'movq (list (Reg 'rsp) (Reg 'rbp)))
+                                 (Instr 'subq (list (Imm 16000) (Reg 'rsp)))
+                                 (Jmp 'start))))))
 
 ; TODO: Fix stack frame size (Assumed to be 16?).
 (define (generate-conclusion)
   (list (cons
-          'conclusion
-          (Block '()
-                 (list (Instr 'addq (list (Imm 16000) (Reg 'rsp)))
-                       (Instr 'popq (list (Reg 'rbp)))
-                       (Retq))))))
+         'conclusion
+         (Block '()
+                (list (Instr 'addq (list (Imm 16000) (Reg 'rsp)))
+                      (Instr 'popq (list (Reg 'rbp)))
+                      (Retq))))))
 
 ;; prelude-and-conclusion : x86 -> x86
 (define (prelude-and-conclusion p)
@@ -321,10 +424,12 @@
 ;; must be named "compiler.rkt"
 (define compiler-passes
   `(("uniquify" ,uniquify ,interp-Lvar)
-     ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar)
-     ("explicate control" ,explicate-control ,interp-Cvar)
-     ("instruction selection" ,select-instructions ,interp-x86-0)
-     ("assign homes" ,assign-homes ,interp-x86-0)
-     ("patch instructions" ,patch-instructions ,interp-x86-0)
-     ("prelude-and-conclusion" ,prelude-and-conclusion ,interp-x86-0)
-     ))
+    ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar)
+    ("explicate control" ,explicate-control ,interp-Cvar)
+    ("instruction selection" ,select-instructions ,interp-x86-0)
+    ("uncover live" ,uncover-live ,interp-x86-0)
+    ("build interference graph" ,build-interference ,interp-x86-0)
+    ("assign homes" ,assign-homes ,interp-x86-0)
+    ("patch instructions" ,patch-instructions ,interp-x86-0)
+    ("prelude-and-conclusion" ,prelude-and-conclusion ,interp-x86-0)
+    ))
